8d2dfed771cac5a892d56b678063531f818e755c
허예진 <yejin097@naver.com>
2025-01-12 20:30:15 +0900

    feat: eslint 추가 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,20 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import eslintConfigPrettier from 'eslint-config-prettier';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const compat = new FlatCompat({
+  files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  languageOptions: { globals: globals.browser },
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  eslintConfigPrettier,
-  {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+  ...compat.config({
     rules: {
       'no-unused-vars': 'warn',
       'no-undef': 'warn',
     },
-  },
+    extends: ['eslint:recommended', 'prettier', 'plugin:@next/next/recommended'],
+  }),
 ];
