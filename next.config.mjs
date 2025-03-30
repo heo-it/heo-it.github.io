8361189338cca1cb862ab8044b928fb733739679
허예진 <yejin097@naver.com>
2025-03-30 22:19:23 +0900

    build: nextjs.config markdown 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
-export default (phase) => {
+import createMDX from '@next/mdx';
+
+const withMDX = createMDX({
+  // TODO - Markdown 플러그인 설정
+});
+
+export default withMDX((phase) => {
   const isDev = phase === process.env.NODE_ENV;
   /**
    * @type {import('next').NextConfig}
@@ -6,6 +12,7 @@ export default (phase) => {
   const nextConfig = {
     output: 'export',
     assetPrefix: isDev ? undefined : 'https://heo-it.github.io/',
+    pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   };
   return nextConfig;
-};
+});
